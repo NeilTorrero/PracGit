2010bf11402b5ea546a3376136add693c56ca3f5
Neil <neiltorrero@gmail.com>
2018-10-04 18:20:51 +0200

    feat: Implementacio del LLISTABIORD_crea
    S'ha implementat la funcio crea de la llista

--- llista.c
+++ llista.c
@@ -1,7 +1,27 @@
 #include <llista.h>
 
 LlistaBiOrd LLISTABIORD_crea(){
-
+  LlistaBiOrd l;
+  l.pri = (NodeBiOrd*) malloc (sizeof(NodeBiOrd));
+  if (l.pri == NULL){
+    printf ("No s'ha pugut crear la llista.\n");
+  }else{
+    l.pri -> e = ELEMENT_indefinit();
+    l.pri -> ant = NULL;
+    l.ult = (NodeBiOrd*) malloc (sizeof(NodeBiOrd));
+    if (l.ult == NULL) {
+      printf ("No s'ha pugut crear la llista.\n");
+      free (l.pri);
+      l.pri = NULL;
+    }else{
+      l.ult -> e = ELEMENT_indefinit();
+      l.ult -> seg = NULL;
+      l.pri -> seg = l.ult;
+      l.ult -> ant = l.pri;
+      l.pri = l.ult;
+    }
+  }
+  return l;
 }
 
 void LLISTABIORD_insereixOrdenat (LlistaBiOrd *l, Element e){
