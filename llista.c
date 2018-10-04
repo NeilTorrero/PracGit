753e00dc6c13de9dc1a764d143d1792b4b21a101
Neil <neiltorrero@gmail.com>
2018-10-04 18:06:34 +0200

    feat: S'ha afegit l'estructura de la llista
    S'ha afegit la estructura dels procediments de la llista del .c per a la seva implementacio

---
+++ llista.c
@@ -0,0 +1,49 @@
+#include <llista.h>
+
+LlistaBiOrd LLISTABIORD_crea(){
+
+}
+
+void LLISTABIORD_insereixOrdenat (LlistaBiOrd *l, Element e){
+
+}
+
+Element LLISTABIORD_consulta (LlistaBiOrd l){
+
+}
+
+void LLISTABIORD_esborra (LlistaBiOrd *l){
+
+}
+
+void LLISTABIORD_avanca (LlistaBiOrd *l){
+
+}
+
+void LLISTABIORD_retrocediex (LlistaBiOrd *l){
+
+}
+
+void LLISTABIORD_vesInici (LlistaBiOrd *l){
+
+}
+
+void LLISTABIORD_vesFi (LlistaBiOrd *l){
+
+}
+
+int LLISTABIORD_inici (LlistaBiOrd l){
+
+}
+
+int LLISTABIORD_fi (LlistaBiOrd l){
+
+}
+
+int LLISTABIORD_buida (LlistaBiOrd l){
+
+}
+
+int LLISTABIORD_destrueix (LlistaBiOrd l){
+
+}
