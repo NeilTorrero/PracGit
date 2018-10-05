d90580ae58f1964732f42dd95f7088e28e60fb09
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-10-06 00:11:06 +0200

    feat: main - Creada estructura
    S'ha creat la estructura per fer el main amb el "testeig" de la llista

--- main.c
+++ main.c
@@ -3,5 +3,30 @@
 #include <llista.h>
 
 void main() {
-  
+  LlistaBiOrd l;
+
+  printf("Creacio llista\n");
+  l = LLISTABIORD_crea();
+
+  LLISTABIORD_insereixOrdenat (LlistaBiOrd *l, Element e);
+
+  //Element LLISTABIORD_consulta (LlistaBiOrd l);
+
+  //void LLISTABIORD_esborra (LlistaBiOrd *l);
+
+  //void LLISTABIORD_avanca (LlistaBiOrd *l);
+
+  //void LLISTABIORD_retrocediex (LlistaBiOrd *l);
+
+  //void LLISTABIORD_vesInici (LlistaBiOrd *l);
+
+  //void LLISTABIORD_vesFi (LlistaBiOrd *l);
+
+  //int LLISTABIORD_inici (LlistaBiOrd l);
+
+  //int LLISTABIORD_fi (LlistaBiOrd l);
+
+  //int LLISTABIORD_buida (LlistaBiOrd l);
+
+  //int LLISTABIORD_destrueix (LlistaBiOrd *l);
 }
