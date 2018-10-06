c1b11336136bf9fe9f956e6f784531fb5cb7b023
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-10-06 23:51:48 +0200

    fix: main - Correcio de nom de variables

--- main.c
+++ main.c
@@ -15,47 +15,47 @@ typedef struct{
 }LlistaBiOrd;
 
 void main() {
-  LlistaBiOrd l;
+  LlistaBiOrd lbo;
   int e, boolea;
   int e_consulta;
 
   printf("Creacio llista\n");
-  l = LLISTABIORD_crea();
+  lbo = LLISTABIORD_crea();
 
   printf("Insercio ordenada a la llista\n");
   e = 12;
-  LLISTABIORD_insereixOrdenat (&l, e);
+  LLISTABIORD_insereixOrdenat (&lbo, e);
 
   printf("Consulta valor element dins la llista\n");
-  e_consulta = LLISTABIORD_consulta (l);
+  e_consulta = LLISTABIORD_consulta (lbo);
   printf("Valor element = %d \n", e_consulta);
 
   printf("Esborra el element on es situa el pdi\n");
-  LLISTABIORD_esborra (&l);
-  e_consulta = LLISTABIORD_consulta (l);
+  LLISTABIORD_esborra (&lbo);
+  e_consulta = LLISTABIORD_consulta (lbo);
   printf("Valor element = %d \n", e_consulta);
 
   printf("Avanca el pdi al seguent element de la llista\n");
-  LLISTABIORD_avanca (&l);
+  LLISTABIORD_avanca (&lbo);
 
   printf("Retrocediex el pdi al anterior element de la llista\n");
-  LLISTABIORD_retrocediex (&l);
+  LLISTABIORD_retrocediex (&lbo);
 
   printf("El pdi es situa al principi de la llista\n");
-  LLISTABIORD_vesInici (&l);
+  LLISTABIORD_vesInici (&lbo);
 
   printf("El pdi es situa al final de la llista\n");
-  LLISTABIORD_vesFi (&l);
+  LLISTABIORD_vesFi (&lbo);
 
   printf("Retorna 1 si el pdi esta al principi de la llista\n");
-  boolea = LLISTABIORD_inici (l);
+  boolea = LLISTABIORD_inici (lbo);
 
   printf("Retorna 1 si el pdi esta al final de la llista\n");
-  boolea = LLISTABIORD_fi (l);
+  boolea = LLISTABIORD_fi (lbo);
 
   printf("Retrona 1 si la llista es buida\n");
-  boolea = LLISTABIORD_buida (l);
+  boolea = LLISTABIORD_buida (lbo);
 
   printf("Destruiex la llista, alliberant memoria\n");
-  boolea = LLISTABIORD_destrueix (&l);
+  boolea = LLISTABIORD_destrueix (&lbo);
 }
