e78bbd62eeb6d50fd845a4333fabb3d5e440ef73
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-10-07 20:44:55 +0200

    doc: C - Afegit comentaris del codi

--- llista.c
+++ llista.c
@@ -1,11 +1,36 @@
+/************************************
+*
+*@Proposit: Implementacio d'una llista
+*bidirrecional ordenada.
+* Interficie modul LLISTABIORD
+*@Autor: Neil Torrero Salas
+*
+* ***********************************/
+
 #include "llista.h"
 
+
+/**********************************************************
+ *
+ * @Finalitat: Crear informacio qualsevol per al fantasma.
+ * @Parametres: ----.
+ * @Retorn: Retorna el registre amb la informacio.
+ *
+ * *******************************************************/
 int ELEMENT_indefinit (){
   int e;
   e = -1;
   return e;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Crear la llista a partir del no res.
+ * @Parametres: int/out: ok = Variable que actua com
+ * 							boleana per els errors.
+ * @Retorn: Retorna la llista amb el fantasma i el punters.
+ *
+ * *******************************************************/
 LlistaBiOrd LLISTABIORD_crea(){
   LlistaBiOrd l;
   l.pri = (NodeBiOrd*) malloc (sizeof(NodeBiOrd));
@@ -31,6 +56,14 @@ LlistaBiOrd LLISTABIORD_crea(){
   return l;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Inseriex l'element de forma ordenada.
+ * @Parametres: in: l = Llista.
+ * 				in: e = Element a inserir.
+ * @Retorn: Retorna la llista amb l'element inserit.
+ *
+ * *******************************************************/
 LlistaBiOrd LLISTABIORD_insereixOrdenat (LlistaBiOrd l, int e){
   NodeBiOrd* aux;
   aux = (NodeBiOrd*) malloc (sizeof(NodeBiOrd));
@@ -52,6 +85,13 @@ LlistaBiOrd LLISTABIORD_insereixOrdenat (LlistaBiOrd l, int e){
   return l;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Consulta l'element indicat al pdi.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna l'element.
+ *
+ * *******************************************************/
 int LLISTABIORD_consulta (LlistaBiOrd l){
   int e = ELEMENT_indefinit();
   if (l.pdi != l.pri && l.pdi != l.ult) {
@@ -60,6 +100,14 @@ int LLISTABIORD_consulta (LlistaBiOrd l){
   return e;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Esborra l'element del pdi,
+ * 			   el seguent passa a ser el pdi.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista amb l'element borrat.
+ *
+ * *******************************************************/
 void LLISTABIORD_esborra (LlistaBiOrd *l){
   NodeBiOrd *aux;
   if (l -> pdi != l -> pri && l -> pdi != l -> ult) {
@@ -71,38 +119,94 @@ void LLISTABIORD_esborra (LlistaBiOrd *l){
   }
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Avanca una casella al pdi.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista amb el pdi modificat.
+ *
+ * *******************************************************/
 void LLISTABIORD_avanca (LlistaBiOrd *l){
   if (l -> pdi != l -> ult) {
     l -> pdi = l -> pdi -> seg;
   }
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Retrocediex una casella al pdi.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista amb el pdi modificat.
+ *
+ * *******************************************************/
 void LLISTABIORD_retrocediex (LlistaBiOrd *l){
   if (l -> pdi != l -> pri){
     l -> pdi = l -> pdi -> ant;
   }
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Posiciona el pdi al inici.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista amb el pdi modificat.
+ *
+ * *******************************************************/
 void LLISTABIORD_vesInici (LlistaBiOrd *l){
   l -> pdi = l -> pri -> seg;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Posiciona el pdi al final.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista amb el pdi modificat.
+ *
+ * *******************************************************/
 void LLISTABIORD_vesFi (LlistaBiOrd *l){
   l -> pdi = l -> ult -> ant;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Comprova si el pdi es al inici de la llista.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna 1 si es al inici, 0 si no.
+ *
+ * *******************************************************/
 int LLISTABIORD_inici (LlistaBiOrd l){
   return l.pdi == l.pri;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Comprova si el pdi es al final de la llista.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna 1 si es al final, 0 si no.
+ *
+ * *******************************************************/
 int LLISTABIORD_fi (LlistaBiOrd l){
   return l.pdi == l.ult;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Comprova si la llista es buida.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna 1 si es buida, 0 si no.
+ *
+ * *******************************************************/
 int LLISTABIORD_buida (LlistaBiOrd l){
   return l.pri -> seg == l.ult;
 }
 
+/**********************************************************
+ *
+ * @Finalitat: Allibera memoria i esborra tots els elements.
+ * @Parametres: in: l = Llista.
+ * @Retorn: Retorna la llista buida.
+ *
+ * *******************************************************/
 void LLISTABIORD_destrueix (LlistaBiOrd *l){
   NodeBiOrd *aux;
   while (l -> pri != NULL) {
